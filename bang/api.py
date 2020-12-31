d8f668474d773648fbba543ec61bc6dd755ff9a5
Nathan Slaughter <nathan.slaughter@q2.com>
2020-12-31 16:26:13 -0600

    Add templated urls (with angle bracket terms)
    This will support things like /hello/{name}

--- bang/api.py
+++ bang/api.py
@@ -5,6 +5,7 @@ This is the core for Bang web apps.
 
 from typing import Dict, Callable, NewType
 
+from parse import parse
 from webob import Request, Response
 
 
@@ -17,10 +18,12 @@ class BangAPI:
     """
     routes: Dict[str, Handler] = {}
 
+
     def __call__(self, environ, start_response):
         request = Request(environ)
         response = self.handle_request(request)
         return response(environ, start_response)
+
 
     def route(self, path: str):
         def wrapper(handler: Handler):
@@ -29,15 +32,33 @@ class BangAPI:
 
         return wrapper
 
-    def handle_request(self, request: Request) -> Response:
+
+    def default_response(self, response):
+        response.status_code = 404
+        response.text = "Not found."
+
+
+    def find_handler(self, request_path):
+        for path, handler in self.routes.items():
+            parse_result = parse(path, request_path)
+            if parse_result is not None:
+                return handler, parse_result.named
+
+        return None, None
+
+
+    def handle_request(self, request):
         response = Response()
 
-        handler = self.routes.get(request.path)
+        handler, kwargs = self.find_handler(request_path=request.path)
+
         if handler is not None:
-            handler(request, response)
+            handler(request, response, **kwargs)
         else:
             self.default_response(response)
+
         return response
+
 
     def default_response(self, response):
         response.status_code = 404
