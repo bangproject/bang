8200044d0641c29a64b3a783840b6f9736bf03cf
Nathan Slaughter <nathan.slaughter@q2.com>
2020-12-31 07:00:44 -0600

    Add example request handler to API

--- bang/api.py
+++ bang/api.py
@@ -5,6 +5,13 @@ class BangAPI(object):
     def __call__(self, environ, start_response):
         request = Request(environ)
 
-        response = Response()
-        response.text = "Hello, World!"
+        response = self.handle_request(request) 
         return response(environ, start_response)
+
+    def handle_request(self, request: Request) -> Response:
+        user_agent = request.environ.get("HTTP_USER_AGENT", "No user agent found.")
+
+        response = Response()
+        response.text = f"Your user-agent is {user_agent}."
+
+        return response
