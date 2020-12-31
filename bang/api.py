062122f61a19a14c5c4bdbed669b9e88f41d45e1
Nathan Slaughter <nathan.slaughter@q2.com>
2020-12-31 14:51:34 -0600

    Refactor request handler in BangAPI

--- bang/api.py
+++ bang/api.py
@@ -31,12 +31,12 @@ class BangAPI:
 
     def handle_request(self, request: Request) -> Response:
         response = Response()
-        for path, handler in self.routes.items():
-            if path == request.path:
-                handler(request, response)
-                return response
 
-        self.default_response(response)
+        handler = self.routes.get(request.path)
+        if handler is not None:
+            handler(request, response)
+        else:
+            self.default_response(response)
         return response
 
     def default_response(self, response):
