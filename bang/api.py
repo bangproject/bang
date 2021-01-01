f511cf4761b8f82a3630ad1e87a19a1f89e2f437
Nathan Slaughter <nathan.slaughter@q2.com>
2021-01-01 08:54:39 -0600

    Add support for class handlers the HTTP verb methods
    Now you can decorate the handler with @app.route("/path") then you
    defined HTTP verb named methods such as `def get(self, ...):` and `def
    post(self, ...):`.

--- bang/api.py
+++ bang/api.py
@@ -2,6 +2,7 @@
 
 This is the core for Bang web apps.
 """
+import inspect
 
 from typing import Dict, Callable, NewType
 
@@ -55,6 +56,10 @@ class BangAPI:
         handler, kwargs = self.find_handler(request_path=request.path)
 
         if handler is not None:
+            if inspect.isclass(handler):
+                handler = getattr(handler(), request.method.lower(), None)
+                if handler is None:
+                    raise AttributeError("Method not allowed", request.method)
             handler(request, response, **kwargs)
         else:
             self.default_response(response)
