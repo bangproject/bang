258b695aab584dea95fe18ce0534383352e9528e
Nathan Slaughter <nathan.slaughter@q2.com>
2021-01-11 22:58:31 -0600

    Sanity check for middleware with app

--- bang/app.py
+++ bang/app.py
@@ -59,3 +59,12 @@ app.add_exception_handler(custom_exception_handler)
 @app.route("/exception")
 def exception_throwing_handler(request, response):
     raise AssertionError("This handler should not be used.")
+
+class SimpleCustomMiddleware(Middleware):
+    def process_request(self, req):
+        print("Processing request", req.url)
+
+    def process_response(self, req, res):
+        print("Processing response", req.url)
+
+app.add_middleware(SimpleCustomMiddleware)
